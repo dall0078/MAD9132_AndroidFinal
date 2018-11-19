04cfcc6622bbd1810ee1c083756683dd56cfa5c2
Junior Dallagnolo <ernilodjr@gmail.com>
2018-11-19 18:40:52 -0500

    Initial Files Commit

---
+++ app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/MovieSearch.kt
@@ -0,0 +1,12 @@
+package com.algonquinlive.groupawesome.mad9132_androidfinal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieSearch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_search)
+    }
+}
