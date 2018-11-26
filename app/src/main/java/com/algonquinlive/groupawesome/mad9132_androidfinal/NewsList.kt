f4b8890e7797dfc2cd86568cb1f009e696e09f56
will0684 <jordan.walcaraz@gmail.com>
2018-11-26 14:50:23 -0500

    Completed query, building list adapter

--- app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/NewsList.kt
+++ app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/NewsList.kt
@@ -1,13 +1,19 @@
 package com.algonquinlive.groupawesome.mad9132_androidfinal
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Bitmap
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_news_list.*
+import kotlinx.android.synthetic.main.news_list_item.*
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
 import java.net.HttpURLConnection
@@ -15,9 +21,11 @@ import java.net.URL
 
 class NewsList : AppCompatActivity() {
 
-    var newsStoryArray = ArrayList<Story>()
+    var newsStoryArray = ArrayList<Story?>()
 
-    data class Story(var title: String?, var author: String?, var date: String?, var link: String?, var content: String?)
+    lateinit var storyAdapter: NewsAdapter
+
+    data class Story(var title: String?, var author: String?, var date: String?, var link: String?, var description: String?)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +33,44 @@ class NewsList : AppCompatActivity() {
 
         Log.d("TEST", "TEST")
 
-        var listItems: ListView?=null
-
-        listItems = this.addedFavouritesListView
+        val listItems: ListView = this.addedFavouritesListView
 
         val myQuery = StoryQuery()
         myQuery.execute()
+
+        storyAdapter = NewsAdapter(this)
+        listItems.adapter = storyAdapter
+    }
+
+    inner class NewsAdapter(ctx: Context): ArrayAdapter<Story>(ctx, 0) {
+
+        override fun getCount(): Int {
+            return newsStoryArray.size
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val inflater = LayoutInflater.from(parent.context)
+            val result: View
+            result = inflater.inflate(R.layout.news_list_item, parent, false)
+            val storyTitle = storyListItemTitle
+            val story = getItem(position)
+            storyTitle.text = story?.title
+
+            return result
+        }
+
+        override fun getItem(position: Int): Story? {
+            return newsStoryArray[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
     }
 
     @SuppressLint("StaticFieldLeak")
     inner class StoryQuery : AsyncTask<String, Int, String>() {
         var story: Story? = null
-        var title: String? = null
-        var author: String? = null
-        var date: String? = null
-        var content: String? = null
         var progress = 0
         lateinit var bitmap: Bitmap
 
@@ -53,27 +84,37 @@ class NewsList : AppCompatActivity() {
             factory.isNamespaceAware = false
             val xpp = factory.newPullParser()
             xpp.setInput(response, "UTF-8")
+            var eventType = xpp.eventType
 
-            while (xpp.eventType != XmlPullParser.END_DOCUMENT) {
-                Log.d("STARTING PARSER", "STARTING PARSER")
-                when(xpp.eventType) {
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                Log.d("PARSING: ", "Event Type: ${xpp.eventType}, Event Name: ${xpp.name}")
+                when(eventType) {
                     XmlPullParser.START_TAG -> {
                         if (xpp.name == "item") {
-                            Log.d("FOUND ITEM TAG", "CREATING STORY CLASS")
+                            Log.d("FOUND ITEM TAG", "Creating Story Object")
                             this.story = Story(null, null,null,null, null)
                         } else if (this.story != null) {
                             when {
-                                xpp.name == "title" -> this.story!!.title = xpp.nextText()
-                                xpp.name == "link" -> this.story!!.link = xpp.nextText()
-                                xpp.name == "pubDate" -> this.story!!.date = xpp.nextText()
-                                xpp.name == "author" -> this.story!!.author == xpp.nextText()
-                                xpp.name == "description" -> this.story!!.content = xpp.nextText()
+                                xpp.name == "title" -> this.story?.title = xpp.nextText()
+                                xpp.name == "link" -> this.story?.link = xpp.nextText()
+                                xpp.name == "pubDate" -> this.story?.date = xpp.nextText()
+                                xpp.name == "author" -> this.story?.author == xpp.nextText()
+                                xpp.name == "description" -> this.story?.description = xpp.nextText()
                             }
+                            Log.d("Added Fields: ", "${this.story?.title}")
                         }
                         publishProgress()
                     }
+                    XmlPullParser.END_TAG -> {
+                        if (xpp.name == "item") {
+                            newsStoryArray.add(this.story)
+                            this.story = null
+                        }
+                    }
                 }
+                eventType = xpp.next()
             }
+            Log.d("Array: ", "$newsStoryArray")
             return "Done"
         }
     }
