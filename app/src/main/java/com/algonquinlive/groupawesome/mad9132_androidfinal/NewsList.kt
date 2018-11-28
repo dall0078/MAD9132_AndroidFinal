6b0de91fa09225e9a97b40cbb7ae7b5b296c6399
will0684 <jordan.walcaraz@gmail.com>
2018-11-28 17:36:06 -0500

    Images and description parsing now working

--- app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/NewsList.kt
+++ app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/NewsList.kt
@@ -2,6 +2,7 @@ package com.algonquinlive.groupawesome.mad9132_androidfinal
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
@@ -19,6 +20,8 @@ import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.regex.Pattern
+
 
 class NewsList : AppCompatActivity() {
 
@@ -26,13 +29,27 @@ class NewsList : AppCompatActivity() {
 
     lateinit var storyAdapter: NewsAdapter
 
-    data class Story(var title: String?, var author: String?, var date: String?, var link: String?, var description: String?)
+    data class Story(var title: String?, var author: String?, var date: String?, var link: String?, var imageLink: String?, var description: String?)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_list)
 
         val listItems: ListView = newsItemsListView
+
+        listItems.setOnItemClickListener {_, _, position, _ ->
+
+            val intent = Intent(this, NewsDetail::class.java)
+
+            intent.putExtra("title",newsStoryArray[position]?.title)
+            intent.putExtra("author",newsStoryArray[position]?.author)
+            intent.putExtra("date",newsStoryArray[position]?.date)
+            intent.putExtra("link",newsStoryArray[position]?.link)
+            intent.putExtra("description",newsStoryArray[position]?.description)
+            intent.putExtra("imageLink",newsStoryArray[position]?.imageLink)
+
+            startActivity(intent)
+        }
 
         val myQuery = StoryQuery()
         myQuery.execute()
@@ -55,6 +72,11 @@ class NewsList : AppCompatActivity() {
             val story = getItem(position)
             storyTitle.text = story?.title
 
+            val articleImageView = result.storyListItemImage
+            val imgSrc = story?.imageLink
+            val htmlString = "<img src='$imgSrc'/>"
+            articleImageView.loadDataWithBaseURL(null, "<style>img{display: inline;height: auto;max-width: 100%;}</style>" + htmlString, "text/html", "UTF-8", null)
+
             return result
         }
 
@@ -72,6 +94,7 @@ class NewsList : AppCompatActivity() {
         var story: Story? = null
         var progress = 0
         lateinit var bitmap: Bitmap
+
 
         override fun doInBackground(vararg params: String?): String {
             val url = URL("https://www.cbc.ca/cmlink/rss-world")
@@ -91,14 +114,18 @@ class NewsList : AppCompatActivity() {
                     XmlPullParser.START_TAG -> {
                         if (xpp.name == "item") {
                             Log.d("FOUND ITEM TAG", "Creating Story Object")
-                            this.story = Story(null, null,null,null, null)
+                            this.story = Story(null, null,null,null, null, null )
                         } else if (this.story != null) {
                             when {
                                 xpp.name == "title" -> this.story?.title = xpp.nextText()
                                 xpp.name == "link" -> this.story?.link = xpp.nextText()
                                 xpp.name == "pubDate" -> this.story?.date = xpp.nextText()
                                 xpp.name == "author" -> this.story?.author == xpp.nextText()
-                                xpp.name == "description" -> this.story?.description = xpp.nextText()
+                                xpp.name == "description" -> {
+                                    this.story?.description = xpp.nextText()
+                                    this.story?.imageLink = PatternMatcherGroupHtml.main(story!!.description!!)
+                                    this.story?.description = PatternMatcherGroupHtmlText.main(story!!.description!!)
+                                }
                             }
                             Log.d("Added Fields: ", "${this.story?.title}")
                         }
@@ -119,6 +146,41 @@ class NewsList : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             storyAdapter.notifyDataSetChanged()
+        }
+    }
+
+    object PatternMatcherGroupHtml {
+
+        @JvmStatic
+        fun main(arg: String): String {
+            val p = Pattern.compile("src='(\\S+)'")
+            val m = p.matcher(arg)
+            // if we find a match, get the group
+            if (m.find()) {
+                // get the matching group
+                val codeGroup = m.group(1)
+                // print the group
+                System.out.format("'%s'\n", codeGroup)
+                return codeGroup
+            }
+            return arg
+        }
+    }
+    object PatternMatcherGroupHtmlText {
+
+        @JvmStatic
+        fun main(arg: String): String {
+            val p = Pattern.compile("<p>(.*?)</p>")
+            val m = p.matcher(arg)
+            // if we find a match, get the group
+            if (m.find()) {
+                // get the matching group
+                val codeGroup = m.group(1)
+                // print the group
+                System.out.format("'%s'\n", codeGroup)
+                return codeGroup
+            }
+            return arg
         }
     }
 }
