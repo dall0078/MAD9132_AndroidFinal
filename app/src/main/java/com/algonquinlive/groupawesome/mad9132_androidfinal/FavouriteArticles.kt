1e690953919d38e9b29f75ec1e437d7535a62ad0
will0684 <jordan.walcaraz@gmail.com>
2018-12-02 12:33:19 -0500

    Created Favourites page

---
+++ app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/FavouriteArticles.kt
@@ -0,0 +1,12 @@
+package com.algonquinlive.groupawesome.mad9132_androidfinal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FavouriteArticles : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite_articles)
+    }
+}
