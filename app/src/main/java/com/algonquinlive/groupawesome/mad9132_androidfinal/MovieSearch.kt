7e1cf958853e6316116ff077b6b9d8141ece38f4
AdamWasHere <32351039+AdamWasHere@users.noreply.github.com>
2018-12-05 11:06:42 -0500

    Updated movieSearch code, created MovieDetailsFragment to display movie data, add a drawable, modified layout XML files

--- app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/MovieSearch.kt
+++ app/src/main/java/com/algonquinlive/groupawesome/mad9132_androidfinal/MovieSearch.kt
@@ -3,21 +3,22 @@ package com.algonquinlive.groupawesome.mad9132_androidfinal
 import android.bluetooth.BluetoothA2dp
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Toast
-import android.widget.Toolbar
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.*
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_movie_saved_list_main_row.*
 import kotlinx.android.synthetic.main.activity_movie_search.*
+import org.w3c.dom.Text
 
 
 //----------------------------------------------------------------//
@@ -33,6 +34,29 @@ class MovieSearch : AppCompatActivity() {
 
     var favoriteMovieListArray = ArrayList<MovieData>()
 
+    //----------- Declare lateinit Variables -------------//
+
+    lateinit var listItem: ListView
+    lateinit var listAdapter: MovieAdapter
+    lateinit var movieTitle: TextView
+    lateinit var movieReleaseDate: TextView
+    lateinit var movieRating: TextView
+    lateinit var movieRuntime: TextView
+    lateinit var movieActors: TextView
+    lateinit var moviePlot: TextView
+    lateinit var moviePosterUrl: TextView
+    lateinit var movieSearchUserInput : String
+
+//    progress bar
+    lateinit var movieSearchProgressBar: ProgressBar
+
+//    database
+    lateinit var movieDB: SQLiteDatabase
+    lateinit var movieResults: Cursor
+    //lateinit var movieDbHelper: MovieDatabaseHelper //this will be an inner class of SQLite
+
+    var movieItemPosition = 0
+
 
 
 
@@ -45,15 +69,39 @@ class MovieSearch : AppCompatActivity() {
                          var moviePlot: String?,
                          var moviePosterUrl: String?)
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_search)
 
-        val addButton = findViewById<Button>(R.id.addMovieButton)
+        val addButton = findViewById<ImageButton>(R.id.addMovieButton)
 
         addButton.setOnClickListener{
             Toast.makeText(this, "So Toasty", Toast.LENGTH_LONG).show()
         }
+
+
+        //NavigationClickHandler(this).initializePage()
+
+            val listItems: ListView = movieSearchListView
+
+        listItems.setOnItemClickListener {_, _, position, _ ->
+
+            val intent = Intent(this, MovieDetails::class.java)
+
+
+        intent.putExtra("title", favoriteMovieListArray[position]?.movieTitle)
+        intent.putExtra("release", favoriteMovieListArray[position]?.movieReleaseDate)
+        intent.putExtra("rating", favoriteMovieListArray[position]?.movieRating)
+        intent.putExtra("runtime", favoriteMovieListArray[position]?.movieRuntime)
+        intent.putExtra("starring", favoriteMovieListArray[position]?.movieActors)
+        intent.putExtra("plot", favoriteMovieListArray[position]?.moviePlot)
+        intent.putExtra("poster", favoriteMovieListArray[position]?.moviePosterUrl)
+
+            startActivity(intent)
+    }
+
 
         //----------- Set ToolBar -------------//
 
@@ -70,6 +118,49 @@ class MovieSearch : AppCompatActivity() {
         drawer.addDrawerListener(toggle)
         toggle.syncState()
         var navView = findViewById<View>(R.id.navigationView)
+
+
+
+
+    }
+
+    //----------- Create menu -------------//
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.movie_list_tool_bar_menu, menu)
+        return true
+    }
+
+    //-----------Handle selected items in menu -------------//
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.favourite_movies_toolbar_menu_button -> {
+                var intent = Intent(this, MovieSavedList::class.java)
+                startActivity(intent)
+            }
+
+            R.id.foodHelpIcon -> {
+                var intent = Intent(this, FoodSearch::class.java)
+                startActivity(intent)
+            }
+
+            R.id.item_cbc -> {
+                var intent = Intent(this, NewsList::class.java)
+                startActivity(intent)
+            }
+
+            R.id.item_movie -> {
+                var intent = Intent(this, MovieSearch::class.java)
+                startActivity(intent)
+            }
+
+            R.id.item_bus -> {
+                var intent = Intent(this, BusSearch::class.java)
+                startActivity(intent)
+            }
+        }
+        return true
     }
 
 
@@ -79,9 +170,11 @@ class MovieSearch : AppCompatActivity() {
 
 
 
-    //----------- Create Adapter -------------//
+    //----------- Create Movie Adapter -------------//
 
     inner class MovieAdapter(ctx: Context) : ArrayAdapter<MovieData>(ctx, 0) {
+
+
 
         override fun getCount(): Int {
             //return size of array
@@ -95,6 +188,9 @@ class MovieSearch : AppCompatActivity() {
 
             result = inflater.inflate(R.layout.activity_movie_search, parent, false)
 
+            //assign values here
+
+
 
             return result
 
@@ -102,4 +198,53 @@ class MovieSearch : AppCompatActivity() {
         }
 
     }
+
+//   ---------------------- Databases ---------------------//
+
+    val DATABASE_NAME = "MovieFavorites.db"
+    val VERSION_NUM= 1
+    val TABLE_NAME= "FavoriteMovieItem"
+    val MOVIEITEMKEY = "MovieItems"
+
+//    Create inner class MovieDatabaseHelper
+
+    inner class MovieDataBaseHelper : SQLiteOpenHelper(this@MovieSearch, DATABASE_NAME, null, VERSION_NUM) {
+
+
+        override fun onCreate(db: SQLiteDatabase?) {
+
+            //creates saved movie data base table
+
+            db?.execSQL("CREATE TABLE $TABLE_NAME (_id INTEGER PRIMARY KEY AUTOINCREMENT, $MOVIEITEMKEY TEXT)")
+            Log.i("MovieDataBaseHelper", "calling OnCreate")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+            //delete old data from assigned database
+            db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+
+            //create new table from assigned database
+            onCreate(db)
+            Log.i("MovieDataBaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + "newVersion" + newVersion)
+        }
+    }
+
+
+        //    delete item from array
+        fun deleteMovieItem(id:Long) {
+
+        movieDB.delete(TABLE_NAME, "_id=$id", null)
+
+        //    find item in table
+        movieResults = movieDB.query(TABLE_NAME, arrayOf("_id", MOVIEITEMKEY),
+        null, null, null, null, null, null)
+
+
+        favoriteMovieListArray.removeAt(movieItemPosition)
+        Log.i("MovieSearch", favoriteMovieListArray.toString())
+
+        //reload list of movies
+        listAdapter.notifyDataSetChanged()
+}
+
 }
